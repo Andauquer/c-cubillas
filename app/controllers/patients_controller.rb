f0e59f9ea9fd7d8a61a07c545d58b604e3259739
Andauquer <geba23@hotmail.com>
2018-01-18 21:49:50 +0000

    Completed edit and delete functions for patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -24,23 +24,12 @@ class PatientsController < ApplicationController
   end
   
   def update
-    @admin = Admin.find(params[:id])
-    if params[:admin][:email].empty?                 
-      @admin.errors.add(:base, "El campo de correo electronico no puede estar vacio.")
+    @patient = Patient.find(params[:id])
+    if @patient.update(patient_params)
+      flash[:success] = "La informacion del paciente ha sido actualizada."
+      redirect_to patient_path(@patient)
+    else
       render 'edit'
-    elsif params[:admin][:password] != params[:admin][:password_confirmation]
-      @admin.errors.add(:base, "Las contraseñas ingresadas no son iguales.")
-      render 'edit'
-    elsif params[:admin][:password_actual].empty?
-      @admin.errors.add(:base, "Debe ingresar su contraseña actual para poder hacer efectivos los cambios.")
-      render 'edit'
-    elsif !@admin.authenticate(params[:admin][:password_actual])
-      @admin.errors.add(:base, "La contraseña actual ingresada no es correcta.")
-      render 'edit'
-    else      
-      @admin.update(admin_params.except(:password_actual))
-      flash[:success] = "Sus credenciales han sido actualizadas."
-      redirect_to credenciales_path
     end
   end
   
@@ -57,6 +46,17 @@ class PatientsController < ApplicationController
   def listar
     #De esta manera especificamos la paginacion en el muestro de articulos
     @patients = Patient.paginate(page: params[:page], per_page: 3)
+  end
+  
+  def destroy
+    @patient = Patient.find(params[:id])
+    @patient.destroy
+    flash[:success] = "El registro del paciente fue eliminado."
+    redirect_to listar_pacientes_path
+  end
+  
+  def edit
+    @patient = Patient.find(params[:id])
   end
   
   private
